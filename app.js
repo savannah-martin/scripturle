4ad17dc44954ef5a12ec57c05bdcac3d0824ec3e
Kenan Casey <kenancasey@gmail.com>
2022-02-02 14:17:26 -0600

    Add /example routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let express = require("express");
-let path = require('path');
+let path = require("path");
 let app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -7,15 +7,66 @@ app.use(express.static(path.join(__dirname, "public")));
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-
 /* GET users listing. */
 app.get("/", function (req, res) {
   res.send("respond with a resource");
 });
 
+let mark = { first: "Mark", last: "Hamill", age: "70" };
+
+app.get("/example/:age", (req, res) => {
+  let ageSentence = "";
+  const age = parseInt(req.params.age);
+
+  if (age > mark.age) {
+    ageSentence = "You're older than Mark Hamill!";
+  } else if (age < mark.age) {
+    ageSentence = "You're younger than Mark Hamill!";
+  } else {
+    ageSentence = "You are Mark Hamill!";
+  }
+  const page = `<html>
+                    <head> </head>
+                    <body> 
+                      <h1> Example </h1>
+                      <p> ${ageSentence} </p>
+                    </body>
+                </html>`;
+
+  //res.render('index', ageSentence);
+
+  res.send(page);
+});
+
+
+app.post('/example', (req,res) => {
+
+  let age = req.body.age;
+
+  if (age > mark.age) {
+    ageSentence = "You're older than Mark Hamill!";
+  } else if (age < mark.age) {
+    ageSentence = "You're younger than Mark Hamill!";
+  } else {
+    ageSentence = "You are Mark Hamill!";
+  }
+  const page = `<html>
+                    <head> </head>
+                    <body> 
+                      <h1> Example </h1>
+                      <p> ${ageSentence} </p>
+                    </body>
+                </html>`;
+
+  //res.render('index', ageSentence);
+
+  res.send(page);
+
+});
+
 const port = process.env.PORT || 3000;
 const hostname = process.env.hostname || "localhost";
 
-app.listen(port, ()=> {
-    console.log(`Running server on http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Running server on http://${hostname}:${port}`);
 });
