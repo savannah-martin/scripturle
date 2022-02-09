6fbaababd0595b2d38ebeb9db00041bdf68f1e73
Kenan Casey <kenancasey@gmail.com>
2022-02-09 13:13:34 -0600

    Added v1 basics; Renamed index v1
    Deleted the routes

--- app.js
+++ app.js
@@ -11,30 +11,30 @@ app.use(express.static(path.join(__dirname, "public")));
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=> {
+app.get('/v1', (req, res)=> {
   res.render('index');
 });
 
-app.post("/", (req, res)=> {
+app.post("/v1", (req, res)=> {
 
-  let secretWord = "HEBREWS".toUpperCase();
+  let secretWord = "hebrews".toUpperCase();
 
   // extract the guess value from the body
   const guess = req.body.guess.toUpperCase();
 
-  let result = "";
+  let result = []; // = computeResult();
 
   if(guess === secretWord) {
     result = ['correct', 'correct', 'correct','correct','correct','correct','correct'];
   }
   else {
-    result = ['incorrect', 'misplaced', 'incorrect','incorrect','incorrect','misplaced','misplaced'];
+    result = ['correct', 'misplaced', 'incorrect','incorrect','incorrect','misplaced','misplaced'];
   }
 
   console.log(result);
 
   // return the guess
-  res.render('index', result);
+  res.render('v1', {result: result} ) ;
 
 });
 
