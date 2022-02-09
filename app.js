537fb1836aae292e8004776c2e4a38773545e79b
Kenan Casey <kenancasey@gmail.com>
2022-02-09 13:29:39 -0600

    Fix bug in app.js;
    Should use v1 as view not index.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/v1', (req, res)=> {
-  res.render('index');
+  res.render('v1');
 });
 
 app.post("/v1", (req, res)=> {
